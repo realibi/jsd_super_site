d0a5a6061a2592cd105159060edc8cceccf71634
Duysenaliyev Alibi <40587377+realibi@users.noreply.github.com>
2022-01-24 15:30:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo_block">
+                <img src="./images/logo.png" alt="">
+            </div>
+            <div class="menu_block">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="action_block"></div>
+        </div>
+
+        <br><br>
+
+        <div class="half-border">
+            <div class="orange-circle">
+
+            </div>
+            
+            <div class="white-block">
+
+            </div>
+        </div>
+
+
+        <video src="">
+            <source src="">
+        </video>
+        
+
+    </div>
+</body>
+</html>
